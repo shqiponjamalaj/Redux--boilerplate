bb4cd6c14befc65467706a140815b517d119f812
root <shqiponjamalaj5@gmail.com>
2020-07-01 16:24:40 -0700

    index.js updated

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import recipeReducer from "./recipeReducer";
-import authReducer from "./authReducer";
-
-export default combineReducers({
-  recipeReducer,
-  authReducer
-});
